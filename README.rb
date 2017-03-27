8e74833345f09957d9237098caed73cdf5eb0506
timchen777 <timchen7@yahoo.com>
2017-03-27 20:25:45 +0800

    NTU MEAN class 275

---
+++ README.rb
@@ -0,0 +1,32 @@
+================ MEAN stck NTU　ｃｌａｓｓ　２７５=====================
+1. Register
+https://mlab.com/home
+https://www.getpostman.com/
+https://developers.facebook.com/
+2. Install Node.js https://nodejs.org/en/
+3 npm:
+下載Node.js module或package來擴充app的功能
+如：
+Mongoose
+Morgan：輸出HTTP request至console
+body-parser：解析body
+Express：幫忙建立website
+4. Use 'npm install <pkg> --save', save it as a dependency in package.json.
+ex: install cat-me package
+https://www.npmjs.com/package/cat-me
+> npm init
+> npm install cat-me
+>>> create app.js under node_modules folder
+-------- app.js -----------
+var cat = require('cat-me');
+console.log(cat());
+----------------------------
+> MEANstack\node_modules\cat-me> node app.js
+>> see Cat picture
+
+
+
+
+
+
+
